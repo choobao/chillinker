bdea9be63cf239b9b988c31ae4d32f7c574ba3a7
julie kim <julie.kim.dev@gmail.com>
2024-04-01 17:39:28 +0900

    🚧 collection read

--- src/collection/collection.controller.ts
+++ src/collection/collection.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { CollectionService } from './collection.service';
+import { AuthGuard } from '@nestjs/passport';
+// import { Collections } from './entities/collections.entity';
 
-@Controller('collection')
-export class CollectionController {}
+@Controller('collections')
+export class CollectionController {
+  constructor(private readonly collectionService: CollectionService) {}
+
+  //   내 컬렉션 목록 조회
+  @UseGuards(AuthGuard('jwt'))
+  @Get('/')
+  async myCollections(@Param('user_id', ParseIntPipe) user_id: number) {
+    const myColList = await this.collectionService.getMyColList(user_id);
+    return await myColList;
+  }
+
+  // 내 컬렉션 상세 조회
+  @UseGuards(AuthGuard('jwt'))
+  @Get('/:collectionId')
+  async myCollection(
+    @Param('collection_id', ParseIntPipe) collection_id: number,
+  ) {
+    const myCol = await this.collectionService.getMyCol(collection_id);
+    return myCol;
+  }
+
+  @Post('/')
+  async addCollection() {
+    const addCol = await this.collectionService.createCol();
+    return addCol;
+  }
+
+  @Get('/bookmark/collections')
+  async bookmarkCollections() {}
+
+  @Get('/bookmark/collections/:collectionId')
+  async bookmarkCollection() {}
+}
--- src/collection/collection.service.ts
+++ src/collection/collection.service.ts
@@ -1,4 +1,58 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { Collections } from './entities/collections.entity';
 
 @Injectable()
-export class CollectionService {}
+export class CollectionService {
+  constructor(
+    @InjectRepository(Collections)
+    private colRepository: Repository<Collections>,
+  ) {}
+
+  // 내 컬렉션 목록 조회
+  async getMyColList(user_id: number) {
+    try {
+      return await this.colRepository.find({
+        where: { user_id },
+        // select: ['title'],
+      });
+    } catch (error) {
+      throw new NotFoundException({ message: '유효한 컬렉션이 아닙니다.' });
+    }
+  }
+
+  // 내 컬렉션 상세 조회
+  async getMyCol(collection_id: number) {
+    try {
+      return await this.colRepository.findOne({
+        where: {
+          id: collection_id,
+        },
+      });
+    } catch (error) {
+      throw new NotFoundException({ message: '유효한 컬렉션이 아닙니다.' });
+    }
+  }
+  async createCol() {} // 컬렉션 생성
+  async updateCol() {} // 컬렉션 수정 _작품 추가, 작품 삭제, 컬렉션 정보 수정
+  async deleteCol() {} // 컬렉션 삭제 _북마크한 컬렉션에서는 북마크 버튼을 다시 누르면 취소되는걸로(라디오버튼 느낌?) 추가 로직 필요 -> 이건 추후에 고려
+
+  // 북마크 컬렉션 목록 조회
+  async getBookmarkColList(user_id: number) {
+    try {
+      return this.colRepository.find({
+        where: {
+          is_bookmarked: true,
+          user_id,
+        },
+      });
+    } catch (error) {
+      throw new NotFoundException({ message: '유효한 컬렉션이 아닙니다.' });
+    }
+  }
+  async getBookmarkCol() {} // 북마크 컬렉션 상세 조회
+
+  async addCol() {} // webContents의 작품 페이지에서 컬렉션 추가 가능한 기능 -> 나중에 작성
+}
