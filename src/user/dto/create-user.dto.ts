5bda89158dc558b52ce45c72875ba690a31ff077
slianzg <slian201248@gmail.com>
2024-04-01 16:59:29 +0900

    ✨ update : Users, Follows API 기능에 swagger 추가

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsEmail,
   IsNotEmpty,
@@ -10,6 +11,7 @@ import {
 export class CreateUserDto {
   @IsEmail()
   @IsNotEmpty({ message: '이메일을 입력해주세요.' })
+  @ApiProperty({ example: 'aaaa1234@gmail.com', description: '이메일' })
   email: string;
 
   @IsString()
@@ -22,22 +24,31 @@ export class CreateUserDto {
   )
   @MinLength(8, { message: '비밀번호는 6글자 이상이어야합니다.' })
   @IsNotEmpty({ message: '비밀번호를 입력해주세요.' })
+  @ApiProperty({ example: 'Aaaa@1234', description: '비밀번호' })
   password: string;
 
   @IsString()
   @IsNotEmpty({ message: '비밀번호 확인을 입력해주세요.' })
+  @ApiProperty({ example: 'Aaaa@1234', description: '비밀번호확인' })
   confirmPassword: string;
 
   @IsString()
   @MinLength(1)
   @IsNotEmpty({ message: '닉네임을 입력해주세요.' })
+  @ApiProperty({ example: '사과', description: '닉네임' })
   nickname: string;
 
   @IsString()
   @IsOptional()
+  @ApiProperty({ example: '갤럭시 씁니다.', description: '소개' })
   intro: string;
 
   @IsString()
   @IsOptional()
+  @ApiProperty({
+    example:
+      'https://img.freepik.com/free-photo/delicious-red-apples-in-studio_23-2150811013.jpg?size=626&ext=jpg&ga=GA1.1.2113030492.1711756800&semt=ais',
+    description: '프로필사진',
+  })
   profileImage: string;
 }
