46b822ad9a2609e26248fc7ec23a343b8ab7367c
slianzg <slian201248@gmail.com>
2024-04-02 19:36:33 +0900

    ♻️ refactor : User, Follow 기능 피드백토대로 2차 수정

--- src/follow/entities/follow.entity.ts
+++ src/follow/entities/follow.entity.ts
@@ -15,16 +15,16 @@ export class Follows {
   id: number;
 
   @Column({ type: 'int', nullable: false })
-  following_id: number;
+  followingId: number;
 
   @Column({ type: 'int', nullable: false })
-  follower_id: number;
+  followerId: number;
 
   @ManyToOne(() => Users, (user) => user.followings, { onDelete: 'CASCADE' })
-  @JoinColumn([{ name: 'following_id', referencedColumnName: 'id' }])
+  @JoinColumn([{ name: 'followingId', referencedColumnName: 'id' }])
   followings: Users;
 
   @ManyToOne(() => Users, (user) => user.followers, { onDelete: 'CASCADE' })
-  @JoinColumn([{ name: 'follower_id', referencedColumnName: 'id' }])
+  @JoinColumn([{ name: 'followerId', referencedColumnName: 'id' }])
   followers: Users;
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -4,6 +4,7 @@ import {
   Controller,
   Delete,
   Get,
+  HttpCode,
   Param,
   Patch,
   Post,
@@ -19,15 +20,17 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { DeleteUserDto } from './dto/delete-user.dto';
 import { AuthGuard } from '@nestjs/passport';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { UserInfo } from 'utils/userinfo.decorator';
+import { Users } from './entities/user.entity';
 
 @ApiTags('USER')
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  //회원가입
   @ApiOperation({ summary: '회원가입' })
   @Post('register')
+  @HttpCode(201)
   async register(@Body() createUserDto: CreateUserDto, @Res() res) {
     try {
       const { password, confirmPassword } = createUserDto;
@@ -35,32 +38,27 @@ export class UserController {
         throw new BadRequestException('비밀번호와 비밀번호확인이 다릅니다.');
       }
 
-      await this.userService.register(createUserDto);
-      return res.status(201).send('회원가입되었습니다. 로그인해주세요.');
+      return await this.userService.register(createUserDto);
     } catch (err) {
       console.error(err);
       return res.status(err.status).send(`${err}`);
     }
   }
 
-  //로그인
   @ApiOperation({ summary: '로그인' })
   @Post('login')
   async login(@Body() loginDto: LoginDto, @Res() res) {
     try {
       const tokens = await this.userService.login(loginDto);
       return res
-        .status(201)
         .cookie('accessToken', `Bearer ${tokens.accessToken}`)
-        .cookie('refreshToken', `Bearer ${tokens.refreshToken}`)
-        .send('로그인되었습니다.');
+        .cookie('refreshToken', `Bearer ${tokens.refreshToken}`);
     } catch (err) {
       console.error(err);
       return res.status(err.status).send(`${err}`);
     }
   }
 
-  //AccessToken 재발급
   @ApiOperation({ summary: 'AccessToken 재발급' })
   @Post('refresh')
   async renewAccessToken(@Req() req, @Res() res) {
@@ -73,94 +71,80 @@ export class UserController {
       const [tokenType, token] = refreshToken.split(' ');
       const newToken = (await this.userService.renewAccessToken(token))
         .accessToken;
-      return res
-        .status(200)
-        .cookie('accessToken', `Bearer ${newToken}`)
-        .send('accessToken이 성공적으로 갱신되었습니다.');
+      return res.cookie('accessToken', `Bearer ${newToken}`);
     } catch (err) {
       console.error(err);
       return res.status(err.status).send(`${err}`);
     }
   }
 
-  //로그아웃
   @ApiOperation({ summary: '로그아웃' })
-  @UseGuards(AuthGuard('jwt')) //필요할까요? 이미 지난 AT나 RT를 가지고있을텐데...
+  @UseGuards(AuthGuard('jwt'))
   @Post('logout')
   async logout(@Res() res) {
     try {
-      return res
-        .status(200)
-        .clearCookie('accessToken')
-        .clearCookie('refreshToken')
-        .send('로그아웃되었습니다.');
+      return res.clearCookie('accessToken').clearCookie('refreshToken');
     } catch (err) {
       console.error(err);
       return res.status(err.status).send(`${err}`);
     }
   }
 
-  //마이페이지 조회
   @ApiOperation({ summary: '마이페이지 조회' })
   @UseGuards(AuthGuard('jwt'))
   @Get('mypage')
-  async getMyInfo(@Req() req, @Res() res) {
+  async getMyInfo(@UserInfo() user: Users, @Res() res) {
     try {
-      const { id } = req.user;
-      const myInfo = await this.userService.getUserInfoById(id);
-      return res.status(200).json({ data: myInfo });
+      const { id } = user;
+      return await this.userService.getUserInfoById(id);
     } catch (err) {
       console.error(err);
       return res.status(err.status).send(`${err}`);
     }
   }
 
-  //회원 정보 수정
   @ApiOperation({ summary: '회원 정보 수정' })
   @UseGuards(AuthGuard('jwt'))
   @Patch('mypage/update')
   async updateMyInfo(
     @Body() updateUserDto: UpdateUserDto,
-    @Req() req,
+    @UserInfo() user: Users,
     @Res() res,
   ) {
     try {
-      const { id } = req.user;
+      const { id } = user;
       await this.userService.updateMyInfo(id, updateUserDto);
-      return res.status(201).send('회원정보 수정이 완료되었습니다.');
     } catch (err) {
       console.error(err);
       return res.status(err.status).send(`${err}`);
     }
   }
 
-  //회원탈퇴
   @ApiOperation({ summary: '회원탈퇴' })
   @UseGuards(AuthGuard('jwt'))
   @Delete('mypage/leave')
-  async leave(@Body() deleteUserDto: DeleteUserDto, @Req() req, @Res() res) {
+  @HttpCode(204)
+  async leave(
+    @Body() deleteUserDto: DeleteUserDto,
+    @UserInfo() user: Users,
+    @Res() res,
+  ) {
     try {
-      const { id } = req.user;
+      const { id } = user;
 
       await this.userService.leave(id, deleteUserDto);
-      return res
-        .status(200)
-        .clearCookie('accessToken')
-        .clearCookie('refreshToken')
-        .send('회원탈퇴되었습니다. GoodBye!');
+      return res.clearCookie('accessToken').clearCookie('refreshToken');
     } catch (err) {
       console.error(err);
       return res.status(err.status).send(`${err}`);
     }
   }
 
-  //타 유저 페이지 조회
   @ApiOperation({ summary: '타 유저 페이지 조회' })
   @Get(':id')
   async getUserInfo(@Param('id') id: number, @Res() res) {
     try {
-      const userInfo = await this.userService.getUserInfoById(id);
-      return res.status(200).json({ data: userInfo });
+      return await this.userService.getUserInfoById(id);
     } catch (err) {
       console.error(err);
       return res.status(err.status).send(`${err}`);
