9aa612f9dc1e2f1280c6a8545fd5c621da87c687
julie kim <julie.kim.dev@gmail.com>
2024-04-02 20:53:08 +0900

    🎨 try/catch delete

--- src/collection/collection.service.ts
+++ src/collection/collection.service.ts
@@ -1,8 +1,4 @@
-import {
-  ConflictException,
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
@@ -19,27 +15,19 @@ export class CollectionService {
 
   // 내 컬렉션 목록 조회
   async getMyColList(userId: number) {
-    try {
-      return await this.colRepository.find({
-        where: { userId },
-        // select: ['title'],
-      });
-    } catch (error) {
-      throw new NotFoundException('유효한 컬렉션이 아닙니다.');
-    }
+    return await this.colRepository.find({
+      where: { userId },
+      // select: ['title'],
+    });
   }
 
   // 내 컬렉션 상세 조회
   async getMyCol(collection_id: number) {
-    try {
-      return await this.colRepository.findOne({
-        where: {
-          id: collection_id,
-        },
-      });
-    } catch (error) {
-      throw new NotFoundException('유효한 컬렉션이 아닙니다.');
-    }
+    return await this.colRepository.findOne({
+      where: {
+        id: collection_id,
+      },
+    });
   }
 
   // 컬렉션 생성
@@ -47,12 +35,8 @@ export class CollectionService {
     createColDto: CreateColDto,
     userId: number,
   ): Promise<Collections> {
-    try {
-      const collection = this.colRepository.create({ ...createColDto, userId });
-      return await this.colRepository.save(collection);
-    } catch (error) {
-      throw new ConflictException('컬렉션 생성에 실패했습니다.');
-    }
+    const collection = this.colRepository.create({ ...createColDto, userId });
+    return await this.colRepository.save(collection);
   }
 
   // 컬렉션 수정 _작품 추가, 작품 삭제, 컬렉션 정보 수정
@@ -60,53 +44,41 @@ export class CollectionService {
     collectionId: number,
     updateColDto: UpdateColDto,
   ): Promise<Collections> {
-    try {
-      const collection = await this.colRepository.findOneBy({
-        id: collectionId,
-      });
-      if (!collection) {
-        throw new NotFoundException('유효한 컬렉션이 아닙니다.');
-      }
-
-      collection.title = updateColDto.title;
-      collection.desc = updateColDto.desc;
-      // 추후 웹컨텐츠 로직 추가 예정
-
-      return await this.colRepository.save(collection);
-    } catch (error) {
-      throw new ConflictException('컬렉션 수정에 실패했습니다.');
+    const collection = await this.colRepository.findOneBy({
+      id: collectionId,
+    });
+    if (!collection) {
+      throw new NotFoundException('유효한 컬렉션이 아닙니다.');
     }
+
+    collection.title = updateColDto.title;
+    collection.desc = updateColDto.desc;
+    // 추후 웹컨텐츠 로직 추가 예정
+
+    return await this.colRepository.save(collection);
   }
 
   //   컬렉션 삭제 _북마크한 컬렉션에서는 북마크 버튼을 다시 누르면 취소되는걸로(라디오버튼 느낌?) 추가 로직 필요 -> 이건 추후에 고려
   async deleteCol(collectionId: number): Promise<void> {
-    try {
-      const collection = await this.colRepository.findOneBy({
-        id: collectionId,
-      });
+    const collection = await this.colRepository.findOneBy({
+      id: collectionId,
+    });
 
-      if (!collection) {
-        throw new NotFoundException('유효한 컬렉션이 아닙니다.');
-      }
-
-      await this.colRepository.remove(collection);
-    } catch (error) {
-      throw new ConflictException();
+    if (!collection) {
+      throw new NotFoundException('유효한 컬렉션이 아닙니다.');
     }
+
+    await this.colRepository.remove(collection);
   }
 
   // 북마크 컬렉션 목록 조회
   async getBookmarkColList(userId: number) {
-    try {
-      return this.colRepository.find({
-        where: {
-          isBookmarked: true,
-          userId,
-        },
-      });
-    } catch (error) {
-      throw new NotFoundException({ message: '유효한 컬렉션이 아닙니다.' });
-    }
+    return this.colRepository.find({
+      where: {
+        isBookmarked: true,
+        userId,
+      },
+    });
   }
   //   async getBookmarkCol() {} // 북마크 컬렉션 상세 조회
 
