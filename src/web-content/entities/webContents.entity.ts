bc5efa100ae0492bce1bdf12fe491bf131caf3fa
su2710 <su27109158@gmail.com>
2024-04-05 12:23:18 +0900

    🎉 add: webContent 서비스, 컨트롤러에 메인 조회 기능 추가

--- src/web-content/entities/webContents.entity.ts
+++ src/web-content/entities/webContents.entity.ts
@@ -7,7 +7,7 @@ import {
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
-import { ContentType } from '../webContent.type';
+import { ContentType } from '../../web-content/webContent.type';
 import { IsEnum, IsInt, IsString } from 'class-validator';
 import { Collections } from '../../collection/entities/collections.entity';
 import { C_reviews } from '../../review/entities/chillinker.reivews.entity';
@@ -16,51 +16,97 @@ import { Bogosips } from './bogosips.entity';
 
 @Entity('webContents')
 export class WebContents {
+  /**
+   * id
+   * @example 1
+   */
   @IsInt()
   @PrimaryGeneratedColumn()
   id: number;
 
+  /**
+   * contentType
+   * @example "웹소설"
+   */
   @IsEnum(ContentType)
   @Column({ type: 'enum', enum: ContentType })
   contentType: ContentType;
 
+  /**
+   * title
+   * @example "화산귀환"
+   */
   @IsString()
   @Column({ type: 'varchar', length: 30 })
   title: string;
 
-  @Column({ type: 'varchar', length: 200 })
-  desc?: string;
+  /**
+   * desc
+   * @example "대 화산파 13대 제자. 천하삼대검수(天下三代劍手). 매화검존(梅花劍尊) 청명(靑明) 천하를 혼란에 빠뜨린 고금제일마 천마(天魔)의 목을 치고 십만대산의 정상에서 영면. 백 년의 시간을 뛰어넘어 아이의 몸으로 다시 살아나다."
+   */
+  @Column({ type: 'varchar', length: 300 })
+  desc: string;
 
+  /**
+   * image
+   * @example "https://comicthumb-phinf.pstatic.net/20230619_210/pocket_1687157855027BFTVg_JPEG/%C8%AD%BB%EA%BC%F6%C1%A4.jpg"
+   */
   @Column({ type: 'varchar' })
   image: string;
 
+  /**
+   * bogosipCount
+   * @example 0
+   */
   @Column({ type: 'int' })
   bogosipCount: number;
 
+  /**
+   * rate
+   * @example 5.0
+   */
   @Column({ type: 'float' })
   rate: number;
 
-  @Column({ type: 'varchar', length: 10 })
-  ageLimit: string;
+  /**
+   * rank
+   * @example {"naver": 1}
+   */
+  @Column({ type: 'json', nullable: true })
+  rank?: JSON;
 
+  /**
+   * author
+   * @example "비가"
+   */
   @Column({ type: 'varchar', length: 20 })
   author: string;
 
-  @Column({ type: 'varchar', length: 10 })
-  status: string;
+  /**
+   * keyword
+   * @example "['무협', '스토리']"
+   */
+  @Column({ type: 'varchar', nullable: true })
+  keyword?: string;
 
-  @Column({ type: 'json', nullable: true })
-  keyword?: JSON;
-
+  /**
+   * category
+   * @example "무협"
+   */
   @Column({ type: 'varchar', length: 20 })
   category: string;
 
-  @Column({ type: 'varchar', length: 70 })
-  term?: string;
-
+  /**
+   * platform
+   * @example {"naver": "https://m.series.naver.com/novel/detail.series?productNo=4130558"}
+   */
   @Column({ type: 'json' })
   platform: JSON;
 
+  /**
+   * pubDate
+   * @example "2019-04-25"
+   */
   @Column({ type: 'date' })
   pubDate: Date;
 
--- src/web-content/web-content.service.ts
+++ src/web-content/web-content.service.ts
@@ -1,4 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { WebContents } from './entities/webContents.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ContentType } from './webContent.type';
 
 @Injectable()
-export class WebContentService {}
+export class WebContentService {
+  constructor(
+    @InjectRepository(WebContents)
+    private readonly webContentRepository: Repository<WebContents>,
+  ) {}
+
+  async findBestWebContents(platform: string, type: ContentType) {
+    try {
+      const bestWebContents = await this.webContentRepository
+        .createQueryBuilder('webContents')
+        .leftJoinAndSelect('webContents.cReviews', 'review')
+        .where(
+          `JSON_EXTRACT(webContents.platform, '$.${platform}') IS NOT NULL`,
+        )
+        .andWhere('webContents.contentType = :type', { type })
+        .andWhere('webContents.rank IS NOT NULL')
+        .groupBy('webContents.id')
+        .select([
+          'webContents.id',
+          'webContents.category',
+          'webContents.title',
+          'webContents.rate',
+        ])
+        .addSelect('COUNT(review.id)', 'reviewCount')
+        .addSelect(`JSON_EXTRACT(webContents.rank, '$.${platform}')`, 'ranking') // 플랫폼 별 랭킹
+        .orderBy('ranking', 'ASC') // ranking에 따라 정렬
+        .getRawMany();
+
+      return bestWebContents;
+    } catch (err) {
+      throw new InternalServerErrorException(err.message);
+    }
+  }
+}
