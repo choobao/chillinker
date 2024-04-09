3e3691c49efaa657afb78cd2e8a9c436040cee60
sumin2710 <154856948+sumin2710@users.noreply.github.com>
2024-04-09 21:15:30 +0900

    🩹 fix: webcontent 모듈 수정

--- src/web-content/web-content.module.ts
+++ src/web-content/web-content.module.ts
@@ -4,9 +4,13 @@ import { WebContentController } from './web-content.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Bogosips } from './entities/bogosips.entity';
 import { WebContents } from './entities/webContents.entity';
+import { Users } from '../user/entities/user.entity';
+import { Collections } from '../collection/entities/collections.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Bogosips, WebContents])],
+  imports: [
+    TypeOrmModule.forFeature([Bogosips, WebContents, Users, Collections]),
+  ],
   providers: [WebContentService],
   controllers: [WebContentController],
   exports: [WebContentService],
