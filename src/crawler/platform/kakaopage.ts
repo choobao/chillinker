38147550049a832070b7f934edd6fd279efddd91
sumin2710 <154856948+sumin2710@users.noreply.github.com>
2024-04-16 17:45:03 +0900

    ⚡️ improve: 크롤링 코드 분리 및 수정

---
+++ src/crawler/platform/kakaopage.ts
@@ -0,0 +1,247 @@
+import axios from 'axios';
+import { ContentType } from 'src/web-content/webContent.type';
+import {
+  ReviewSortType,
+  Type,
+  kakao_api_url,
+  webContent_all_query,
+  webcontent_keyword_query,
+  webcontent_query,
+  webcontent_ranking_daily_query,
+  webcontent_review_query,
+} from '../utils/kakaopage.constants';
+
+class KakaopageAxios {
+  // 50위까지의 랭킹 정보 요청
+  async requestDailyRanking(contentType: Type) {
+    try {
+      const { data } = await axios({
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+          Referer: 'https://page.kakao.com',
+        },
+        url: kakao_api_url,
+        data: {
+          query: webcontent_ranking_daily_query,
+          variables: {
+            sectionId: `static-landing-Ranking-section-Landing-${contentType}-0-daily`,
+            param: {
+              categoryUid: contentType,
+              rankingType: 'daily',
+              subcategoryUid: '0',
+              screenUid: null,
+              page: 0,
+            },
+          },
+        },
+      });
+      return data.data.staticLandingRankingSection.groups[0].items;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async requestWebContent(id: number) {
+    try {
+      const { data } = await axios({
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+          Referer: 'https://page.kakao.com',
+        },
+        url: kakao_api_url,
+        data: {
+          query: webcontent_query,
+          operationName: 'contentHomeOverview',
+          variables: { seriesId: id },
+        },
+      });
+      const webContent = data.data.contentHomeOverview.content;
+      const url = `https://page.kakao.com/content/${id}`;
+      const title = webContent.title;
+      const desc = webContent.description;
+      const image = `https:${webContent.thumbnail}`;
+      const contentType =
+        webContent.category === '웹툰'
+          ? ContentType.WEBTOON
+          : ContentType.WEBNOVEL;
+      const category = webContent.subcategory;
+      //const author = webContent.authors;
+      const isAdult = webContent.ageGrade === 'Nineteen' ? 1 : 0;
+      const pubDate = new Date(webContent.startSaleDt);
+
+      const { author, keyword } =
+        await this.requestWebContentKeywordsAndRecommends(id);
+
+      let reviewCount = webContent.serviceProperty.commentCount;
+      reviewCount = reviewCount > 30 ? 30 : reviewCount;
+
+      let reviewList = [];
+      const pageCount = Math.ceil(reviewCount / 25); // 한 페이지당 25개 리뷰 존재할때 필요한 페이지 수
+      for (let i = 1; i <= pageCount; i++) {
+        const reviews = await this.requestWebContentReviews(id, i);
+        reviewList = reviewList.concat(reviews);
+      }
+
+      return {
+        platform: { kakao: url },
+        title,
+        desc,
+        image,
+        contentType,
+        category,
+        author,
+        isAdult,
+        pubDate,
+        keyword,
+        pReviews: reviewList,
+      };
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  // 작품의 키워드와 추천작 정보 요청
+  async requestWebContentKeywordsAndRecommends(id: number) {
+    try {
+      const { data } = await axios({
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+          Referer: 'https://page.kakao.com',
+        },
+        url: kakao_api_url,
+        data: {
+          query: webcontent_keyword_query,
+          variables: { seriesId: id },
+        },
+      });
+      const authors = data.data.contentHomeInfo.about.authorList.map(
+        (author) => {
+          if (author.role === 'writer') {
+            return `author/${author.name}`;
+          } else {
+            return `${author.role}/${author.name}`;
+          }
+        },
+      );
+
+      const author = JSON.stringify(authors)
+        .replace(/\[|\]|\"|\"/g, '')
+        .replace(/author\/([^,]+),illustrator\/\1/g, 'Author/$1');
+
+      const keywords = data.data.contentHomeInfo.about.themeKeywordList.map(
+        (keyword) => keyword.title,
+      );
+
+      const keyword = JSON.stringify(keywords)
+        .replace(/\[|\]|\"|\"/g, '')
+        .replace(/\,/g, ', ');
+
+      const recommends = data.data.contentHomeInfo.recommend.list.items; // 나중에 쓰일지도
+      return { keyword, author };
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  // 작품의 댓글 정보 요청(한 페이지당 25개씩)
+  async requestWebContentReviews(
+    id: number,
+    page: number = 1,
+    sortType: ReviewSortType = ReviewSortType.LIKE,
+  ) {
+    try {
+      const { data } = await axios({
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+          Referer: 'https://page.kakao.com',
+        },
+        url: kakao_api_url,
+        data: {
+          query: webcontent_review_query,
+          variables: {
+            commentListInput: { seriesId: id, page, sortType },
+          },
+        },
+      });
+      const reviews = data.data.commentList.commentList.map((comment) => ({
+        writer: comment.userName,
+        content: comment.comment,
+        likeCount: comment.likeCount,
+        createDate: new Date(comment.createDt),
+      }));
+      return reviews;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async getDailyRank_20_WebContents(contentType: Type) {
+    let items = await this.requestDailyRanking(contentType);
+    const ids = items.slice(0, 20).map((item) => ({
+      id: +item.eventLog.eventMeta.series_id,
+      rank: +item.rank,
+    }));
+
+    const webContentList = [];
+    for (const { id, rank } of ids) {
+      const webContent = await this.requestWebContent(id);
+      webContentList.push({ ...webContent, rank: { kakao: rank } });
+    }
+
+    return webContentList;
+  }
+
+  async requestAllLatestWebContents(contentType: Type, page: number = 1) {
+    try {
+      const { data } = await axios({
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+          Referer: 'https://page.kakao.com',
+        },
+        url: kakao_api_url,
+        data: {
+          query: webContent_all_query,
+          variables: {
+            sectionId: `static-landing-Genre-section-Layout-${contentType}-0-latest-false-82`,
+            param: {
+              categoryUid: contentType,
+              sortType: 'latest',
+              isComplete: false,
+              subcategoryUid: '0',
+              screenUid: 82,
+              page,
+            },
+          },
+        },
+      });
+      return data.data.staticLandingGenreSection.groups[0].items;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async getAll_96_WebContents(contentType: Type, currPage: number = 0) {
+    const webContentList = [];
+    for (let i = 1; i <= 4; i++) {
+      // 한 page당 24개 작품 존재
+      const items = await this.requestAllLatestWebContents(
+        contentType,
+        currPage + i,
+      );
+      const ids = items.map((item) => item.seriesId);
+      for (const id of ids) {
+        const webContent = await this.requestWebContent(id);
+        webContentList.push(webContent);
+      }
+    }
+
+    return webContentList;
+  }
+}
+
+export default KakaopageAxios;
--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -1,33 +1,27 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { RedisClient } from 'ioredis/built/connectors/SentinelConnector/types';
-//import { Redis } from 'ioredis';
-import { RedisClientType, createClient } from 'redis';
+import { Redis } from 'ioredis';
 @Injectable()
-export class RedisService implements OnModuleInit {
-  private readonly client: RedisClientType;
+export class RedisService {
+  private readonly client: Redis;
   constructor(private readonly configService: ConfigService) {
-    this.client = createClient({
+    this.client = new Redis({
+      host: this.configService.get<string>('REDIS_HOST'),
+      port: this.configService.get<number>('REDIS_PORT'),
       password: this.configService.get<string>('REDIS_PASSWORD'),
       username: this.configService.get<string>('REDIS_USERNAME'),
-      socket: {
-        host: this.configService.get<string>('REDIS_HOST'),
-        port: this.configService.get<number>('REDIS_PORT'),
-      },
-      legacyMode: true,
     });
-    this.client.on('error', (err) => console.log('Redis Client Error', err));
   }
 
-  async onModuleInit() {
-    await this.client.connect();
+  getClient(): Redis {
+    return this.client;
   }
 
   async save(key: string, value: any, expiresInSec?: number) {
     if (expiresInSec) {
-      await this.client.setEx(key, expiresInSec, value);
+      await this.client.setex(key, expiresInSec, value);
     } else {
-      await this.client.setEx(key, 24 * 3600, value); // 24h expire
+      await this.client.set(key, value);
     }
   }
 
