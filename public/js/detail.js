4fbb8db62c09f27f8a0532603e957b5825ad586e
choobao <154485888+choobao@users.noreply.github.com>
2024-04-18 03:16:47 +0900

    :sparkles: content detail page can write review & get c-reviews

--- public/js/detail.js
+++ public/js/detail.js
@@ -2,7 +2,8 @@ $(document).ready(function () {
   $('#myform').submit(function (e) {
     e.preventDefault(); // 폼 기본 제출 막기
 
-    console.log('dd');
+    var url = window.location.href;
+    var idUrl = url.split('?')[0];
 
     // 폼 데이터 수집
     var formData = {
@@ -14,15 +15,14 @@ $(document).ready(function () {
     // AJAX 요청
     $.ajax({
       type: 'POST',
-      url: '/books/<%- content.id %>', // 실제 서버 URL로 대체
-      data: formData,
-      success: function (response) {
-        // 성공적으로 데이터를 전송한 후의 처리
-        console.log('서버 응답:', response);
+      url: idUrl,
+      contentType: 'application/json', // 요청의 Content-Type을 application/json으로 설정
+      data: JSON.stringify(formData),
+      success: function (data) {
+        location.reload(true);
       },
-      error: function (xhr, status, error) {
-        // 오류 처리
-        console.error('오류 발생:', error);
+      error: function (response) {
+        alert(response.responseJSON.message);
       },
     });
   });
--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -6,10 +6,17 @@ import { CReviews } from './entities/chillinker.reviews.entity';
 import { PReviews } from './entities/platform.reviews.entity';
 import { ReviewLikes } from './entities/review.likes.entity';
 import { WebContents } from 'src/web-content/entities/webContents.entity';
+import { Users } from 'src/user/entities/user.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([CReviews, PReviews, ReviewLikes, WebContents]),
+    TypeOrmModule.forFeature([
+      CReviews,
+      PReviews,
+      ReviewLikes,
+      WebContents,
+      Users,
+    ]),
   ],
   controllers: [ReviewController],
   exports: [ReviewService],
