fa2acd15a7d900e9d6b848f2ad365a5ec45cf70b
sumin2710 <154856948+sumin2710@users.noreply.github.com>
2024-04-18 13:38:22 +0900

    ✨ 미스터블루 리뷰 크롤링 부분 author -> contentType으로 수정

--- src/web-content/web-content.service.spec.ts
+++ src/web-content/web-content.service.spec.ts
@@ -1,10 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { WebContentService } from './web-content.service';
+import { Repository } from 'typeorm';
+import { WebContents } from './entities/webContents.entity';
+import { Users } from '../user/entities/user.entity';
+import { Collections } from '../collection/entities/collections.entity';
 
 describe('WebContentService', () => {
   let service: WebContentService;
+  let webContentRepository: Partial<Record<keyof Repository<WebContents>, jest.Mock>>;
+  let userRepository: Partial<Record<keyof Repository<Users>, jest.Mock>>;
+  let collectionRepository: Partial<Record<keyof Repository<Collections>, jest.Mock>>;  
 
   beforeEach(async () => {
+    webContentRepository = {
+      createQueryBuilder: jest.fn().mockReturnValue({
+        leftJoinAndSelect:jest.fn().mockReturnThis(),
+        where: jest.fn().mockReturnThis(),
+        andWhere: jest.fn().mockReturnThis(),
+        orWhere: jest.fn().mockReturnThis(),
+        groupBy: jest.fn().mockReturnThis(),
+        select: jest.fn().mockReturnThis(),
+        addSelect: jest.fn().mockReturnThis(),
+        orderBy: jest.fn().mockReturnThis(),
+        getRawMany: jest.fn().mockReturnValue([]),
+        getMany: jest.fn().mockReturnValue([])
+      })
+    }
+
     const module: TestingModule = await Test.createTestingModule({
       providers: [WebContentService],
     }).compile();
