e7c62daa1d21da625a0e37198ad807e6b314c39a
julie kim <julie.kim.dev@gmail.com>
2024-04-23 21:39:08 +0900

    :fire: 중복코드 제거

--- src/collection/dto/updateCol.dto.ts
+++ src/collection/dto/updateCol.dto.ts
@@ -28,13 +28,4 @@ export class UpdateColDto {
   @IsInt({ message: '컨텐츠 아이디를 숫자로 입력해주세요.' })
   @ApiProperty({ example: '1', description: '웹컨텐츠 아이디' })
   webContentId?: number;
-
-  @IsString()
-  @IsOptional()
-  @ApiProperty({
-    example:
-      'https://dn-img-page.kakao.com/download/resource?kid=Du5oG/hzVqEy9QjX/lN2aZRgLz8pegvqODVjfE1&amp;filename=th3',
-    description: '컬렉션 표지 이미지',
-  })
-  coverImage?: string;
 }
