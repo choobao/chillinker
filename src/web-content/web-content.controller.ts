ac630a14080413a6baa83e319b535e313fe385e9
sumin2710 <154856948+sumin2710@users.noreply.github.com>
2024-04-23 22:10:28 +0900

    ✨ 메인페이지, 검색 페이지 성인작품 블라인드 처리

--- src/web-content/web-content.controller.ts
+++ src/web-content/web-content.controller.ts
@@ -1,47 +1,57 @@
-import { Controller, Get, Query, Render } from '@nestjs/common';
+import { Controller, Get, Query, Render, Req, UseGuards } from '@nestjs/common';
 import { WebContentService } from './web-content.service';
 import { ApiOperation } from '@nestjs/swagger';
 import { ContentType } from './webContent.type';
+import { OptionalAuthGuard } from '../auth/optinal.authguard';
 
 @Controller()
 export class WebContentController {
   constructor(private readonly webContentService: WebContentService) {}
 
   @ApiOperation({ summary: '메인' })
+  @UseGuards(OptionalAuthGuard)
   @Get('main')
   @Render('main')
-  async getBestWebContents() {
+  async getBestWebContents(@Req() req) {
     const naverWebtoons = await this.webContentService.findBestWebContents(
       'naver',
       ContentType.WEBTOON,
+      req.user,
     );
     const naverWebnovels = await this.webContentService.findBestWebContents(
       'naver',
       ContentType.WEBNOVEL,
+      req.user,
     );
     const ridiWebtoons = await this.webContentService.findBestWebContents(
       'ridibooks',
       ContentType.WEBTOON,
+      req.user,
     );
     const ridiWebnovels = await this.webContentService.findBestWebContents(
       'ridibooks',
       ContentType.WEBNOVEL,
+      req.user,
     );
     const mrblueWebtoons = await this.webContentService.findBestWebContents(
       'mrblue',
       ContentType.WEBTOON,
+      req.user,
     );
     const mrblueWebnovels = await this.webContentService.findBestWebContents(
       'mrblue',
       ContentType.WEBNOVEL,
+      req.user,
     );
     const kakaoWebtoons = await this.webContentService.findBestWebContents(
       'kakao',
       ContentType.WEBTOON,
+      req.user,
     );
     const kakaoWebnovels = await this.webContentService.findBestWebContents(
       'kakao',
       ContentType.WEBNOVEL,
+      req.user,
     );
 
     return {
@@ -70,9 +80,14 @@ export class WebContentController {
   //   return { users, collections, webnovels, webtoons, authors };
   // }
 
+  @UseGuards(OptionalAuthGuard)
   @Get('search')
   @Render('search')
-  async search(@Query('query') query: string, @Query('type') type: string) {
+  async search(
+    @Req() req,
+    @Query('query') query: string,
+    @Query('type') type: string,
+  ) {
     const regex = /^[a-zA-Z0-9ㄱ-ㅎㅏ-ㅣ가-힣]+$/;
 
     const keyword = regex.test(query)
@@ -80,7 +95,8 @@ export class WebContentController {
       : query.replace(/[^a-zA-Z0-9ㄱ-ㅎㅏ-ㅣ가-힣]/g, '') || 'chillinker';
 
     const { webtoons, webnovels } =
-      (await this.webContentService.searchFromWebContents(keyword)) ?? {};
+      (await this.webContentService.searchFromWebContents(keyword, req.user)) ??
+      {};
 
     if (type == 'webtoons') {
       return { type, keyword, webtoons };
--- src/web-content/web-content.service.ts
+++ src/web-content/web-content.service.ts
@@ -9,6 +9,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { ContentType } from './webContent.type';
 import { Users } from '../user/entities/user.entity';
 import { Collections } from '../collection/entities/collections.entity';
+import _ from 'lodash';
 
 @Injectable()
 export class WebContentService {
@@ -21,7 +22,7 @@ export class WebContentService {
     private readonly collectionRepository: Repository<Collections>,
   ) {}
 
-  async findBestWebContents(platform: string, type: ContentType) {
+  async findBestWebContents(platform: string, type: ContentType, user) {
     try {
       const bestWebContents = await this.webContentRepository
         .createQueryBuilder('webContents')
@@ -41,6 +42,7 @@ export class WebContentService {
           'webContents.title AS title',
           'webContents.image AS image',
           'webContents.author AS author',
+          'webContents.isAdult AS isAdult',
         ])
         .addSelect('COUNT(pReview.id)', 'pReviewCount')
         .addSelect('COUNT(cReview.id)', 'cReviewCount')
@@ -48,10 +50,39 @@ export class WebContentService {
         .orderBy('ranking', 'ASC') // ranking에 따라 정렬
         .getRawMany();
 
-      return bestWebContents;
+      return this.blindAdultImage(user, bestWebContents);
     } catch (err) {
       throw new InternalServerErrorException(err.message);
     }
+  }
+
+  isOver19(birthDate: Date) {
+    const today = new Date();
+    const date19YearsAgo = new Date(
+      today.getFullYear() - 19,
+      today.getMonth(),
+      today.getDate(),
+    );
+    return birthDate <= date19YearsAgo;
+  }
+
+  blindAdultImage(user, contents: WebContents[]) {
+    if (
+      user === false ||
+      _.isNil(user) ||
+      _.isNil(user.birthDate) ||
+      !this.isOver19(new Date(user.birthDate))
+    ) {
+      const adult_image =
+        'https://ssl.pstatic.net/static/m/nstore/thumb/19/home_book_4.png';
+      contents.map((content) => {
+        if (content.isAdult) {
+          content.image = adult_image;
+        }
+        return content;
+      });
+    }
+    return contents;
   }
 
   async searchFromUsers(keyword: string) {
@@ -83,8 +114,8 @@ export class WebContentService {
     return webContents;
   }
 
-  async searchFromWebContents(keyword: string) {
-    const webContents = await this.webContentRepository
+  async searchFromWebContents(keyword: string, user) {
+    let webContents = await this.webContentRepository
       .createQueryBuilder('webContents')
       .where('webContents.title LIKE :keyword', { keyword: `%${keyword}%` })
       .orWhere('webContents.desc LIKE :keyword', { keyword: `%${keyword}%` })
@@ -95,6 +126,7 @@ export class WebContentService {
         keyword: `%${keyword}%`,
       })
       .getMany();
+    webContents = this.blindAdultImage(user, webContents);
 
     const webnovels = webContents.filter(
       (webContent) => webContent.contentType === ContentType.WEBNOVEL,
@@ -110,10 +142,6 @@ export class WebContentService {
   }
 
   async findContent(id: number, type: ContentType) {
-    // const content = await this.webContentRepository
-    //   .createQueryBuilder('webContents')
-    //   .leftJoinAndSelect('webContents.bogosips', 'bogosips')
-    //   .where('webContents.id LIKE :id', { id }).getOne;
 
     const content = await this.webContentRepository.findOne({
       where: { id, contentType: type },
