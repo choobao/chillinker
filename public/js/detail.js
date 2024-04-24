5ac4afd1d662f61c96ab572ada268ecdda76f740
choobao <154485888+choobao@users.noreply.github.com>
2024-04-24 17:03:13 +0900

    :ambulance: content like hot fix

--- public/js/detail.js
+++ public/js/detail.js
@@ -245,6 +245,22 @@ $(document).ready(function () {
   });
 });
 
+$('#like-btn').click(function () {
+  var contentId = $('#content-id').val();
+
+  $.ajax({
+    url: `/likes/change?contentId=${contentId}`,
+    type: 'POST',
+    data: $(this).serialize(),
+    success: function (data) {
+      location.reload(true);
+    },
+    error: function (response) {
+      alert(response.responseJSON.message);
+    },
+  });
+});
+
 // function getCollectionTitles(userId) {
 //   $('#modal').show();
 //   $('#collection-modal').show();
