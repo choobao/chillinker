3f8f6372f26bb169e6101c1f88f9baf7ecc4e2ed
choobao <154485888+choobao@users.noreply.github.com>
2024-04-26 00:04:05 +0900

    :sparkles: if accessToken expired, use refreshToken

--- public/js/click.js
+++ public/js/click.js
@@ -7,3 +7,35 @@ $(document).ready(function () {
     window.location.replace('/search?query=' + input);
   });
 });
+
+$(document).ready(function () {
+  const accessToken = document.cookie
+    .split(';')
+    .map((item) => item.trim())
+    .find((item) => item.startsWith('accessToken='))
+    .split('=')[1];
+
+  const token = accessToken.replace(/^Bearer%20/, '');
+
+  const base64Payload = token.split('.')[1];
+  const decodedPayload = atob(base64Payload);
+  const result = JSON.parse(decodedPayload);
+
+  const expirationTime = new Date(result.exp * 1000).getTime();
+  const issuedAtTime = new Date(result.iat * 1000).getTime();
+  const currentTime = new Date().getTime();
+
+  //accessToken 만료시
+  if (currentTime >= expirationTime) {
+    $.ajax({
+      type: 'POST',
+      url: `/users/refresh`,
+      success: function (datas) {
+        location.reload(true);
+      },
+      error: function (response) {
+        alert(response.responseJSON.message);
+      },
+    });
+  }
+});
